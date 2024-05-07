329a70cff7614866de9079bcb4d206399a2d06a8
Yurii Kyrpotenko <50027889+WeiZedInc@users.noreply.github.com>
2024-05-08 00:31:39 +0300

    added XTEA implemetation

---
+++ Algo/XTEAImpl.cs
@@ -0,0 +1,48 @@
+﻿namespace CryptoLab_Course3.Algo
+{
+    internal class XTEAImpl
+    {
+        private const int NumRounds = 32;
+        private uint[] key;
+
+        public XTEAImpl(byte[] keyBytes)
+        {
+            if (keyBytes.Length != 16)
+                throw new ArgumentException("Ключ має бути довжиною у 128 біт.", nameof(keyBytes));
+
+            key = new uint[4];
+            Buffer.BlockCopy(keyBytes, 0, key, 0, 16);
+        }
+
+        public byte[] Encrypt(byte[] data)
+        {
+            if (data.Length % 8 != 0)
+                throw new ArgumentException("Повідомлення має бути кратним 8.", nameof(data));
+
+            byte[] encrypted = new byte[data.Length];
+            for (int p = 0; p < data.Length; p += 8)
+            {
+                uint[] block = { BitConverter.ToUInt32(data, p), BitConverter.ToUInt32(data, p + 4) };
+                Encode(block);
+                Buffer.BlockCopy(block, 0, encrypted, p, 8);
+            }
+            return encrypted;
+        }
+
+        private void Encode(uint[] v)
+        {
+            uint v0 = v[0], v1 = v[1];
+            uint sum = 0;
+            uint delta = 0x9E3779B9;
+
+            for (int i = 0; i < NumRounds; i++)
+            {
+                v0 += ((v1 << 4) ^ (v1 >> 5)) + v1 ^ (sum + key[sum & 3]);
+                sum += delta;
+                v1 += ((v0 << 4) ^ (v0 >> 5)) + v0 ^ (sum + key[(sum >> 11) & 3]);
+            }
+
+            v[0] = v0; v[1] = v1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,9 +8,12 @@ namespace CryptoLab_Course3
         static void Main(string[] args)
         {
             Console.WriteLine("Введiть рядок для хешування:");
-            string input = Console.ReadLine();
+            //string input = Console.ReadLine();
+            string input = "cocos";
 
             #region Adler32Lab1
+            // https://hash.online-convert.com/ru/adler32-generator -для перевірки
+
             uint myAdler32 = Adler32Impl.ComputeAdler32(Encoding.UTF8.GetBytes(input));
 
             byte[] hashBytes = BitConverter.GetBytes(myAdler32);
@@ -22,6 +25,20 @@ namespace CryptoLab_Course3
             Console.WriteLine($"Мiй хеш по Adler32(uint): {myAdler32}");
             Console.WriteLine($"Мiй хеш по Adler32(hex): {hex}");
             Console.WriteLine($"Мiй хеш по Adler32(base64): {Convert.ToBase64String(hashBytes)}");
+            #endregion
+
+            Console.WriteLine("----------------------------");
+
+            #region XTEALab2
+            // https://asecuritysite.com/encryption/xtea -для перевірки
+
+            byte[] key = Encoding.UTF8.GetBytes("0123456789012345"); // довжина має дорівнювати 128-и бітам.
+            byte[] plaintext = Encoding.UTF8.GetBytes("ABCDEFGH"); //довжина має бути кратною 8-ми
+
+            XTEAImpl xtea = new XTEAImpl(key);
+            byte[] ciphertext = xtea.Encrypt(plaintext);
+            
+            Console.WriteLine($"Мiй хеш по XTEA: {BitConverter.ToString(ciphertext)}");
             #endregion
         }
     }
