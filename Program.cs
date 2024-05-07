f1336b735f119f78d5d98a77fa15e345cdd207b0
Yurii Kyrpotenko <50027889+WeiZedInc@users.noreply.github.com>
2024-05-07 23:19:28 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CryptoLab_Course3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
