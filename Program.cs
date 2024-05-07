d3d43df80f72c100b1c635e660be4bd642fad49d
Yurii Kyrpotenko <50027889+WeiZedInc@users.noreply.github.com>
2024-05-08 00:09:52 +0300

    implemetation of Adler32

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace CryptoLab_Course3
+﻿using CryptoLab_Course3.Algo;
+using System.Text;
+
+namespace CryptoLab_Course3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введiть рядок для хешування:");
+            string input = Console.ReadLine();
+
+            #region Adler32Lab1
+            uint myAdler32 = Adler32Impl.ComputeAdler32(Encoding.UTF8.GetBytes(input));
+
+            byte[] hashBytes = BitConverter.GetBytes(myAdler32);
+            if (BitConverter.IsLittleEndian) // Ставимо байти у правильний порядок
+                Array.Reverse(hashBytes);
+
+            string hex = BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+              
+            Console.WriteLine($"Мiй хеш по Adler32(uint): {myAdler32}");
+            Console.WriteLine($"Мiй хеш по Adler32(hex): {hex}");
+            Console.WriteLine($"Мiй хеш по Adler32(base64): {Convert.ToBase64String(hashBytes)}");
+            #endregion
         }
     }
 }
